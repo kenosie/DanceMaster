8078b4e04ea213d710a8603a184827605ae6004a
Michael Ma <mma01us@gmail.com>
2017-03-25 22:34:44 -0400

    Added INCOMPLETE dance panel

---
+++ NewDance.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import javax.swing.ImageIcon;
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+public class NewDance extends JPanel
+{
+   public NewDance()
+   {
+      private static final int framex = 768;
+      private static final int framey = 1024;
+      int buttonPress = 0;
+      
+      JButton selectSong = new JButton();
+      JFileChooser fc = new JFileChooser();
+      fc.setCurrentDirectory(new java.io.File("C:/Users"));
+   }
+}
