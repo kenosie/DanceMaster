4ed04c5047a1f0495c15dc4ba53fbfa57dd31f4e
Matthew Chen <mchen2001@gmail.com>
2017-03-25 22:56:45 -0400

    Constructed media player

--- DispRes.java
+++ DispRes.java
@@ -6,6 +6,13 @@ import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 
+import com.sun.jna.NativeLibrary;
+
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+import uk.co.caprica.vlcj.player.embedded.videosurface.CanvasVideoSurface;
+import uk.co.caprica.vlcj.runtime.windows.WindowsCanvas;
+
 public class DispRes extends JPanel
 {
    private static final int framex = 768;
@@ -17,5 +24,23 @@ public class DispRes extends JPanel
    int buttonPress = 0; //1 = MainMenu Panel 2 = New Dance
    public DispRes()
    {
+      //create canvas, add to panel
+      WindowsCanvas canvas = new WindowsCanvas();
+      panel.add(canvas);
+      panel.revalidate();
+      panel.repaint();
+      
+      //create media player
+      MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
+      EmbeddedMediaPlayer mediaPlayer = new EmbeddedMediaPlayer();
+      CanvasVideoSurface videoSurface = mediaPlayerFactory.newVideoSurface(canvas);
+      mediaPlayer.setVideoSurface(videoSurface);
    }
 }
+
+/*
+to do:
+display dance moves over music
+display next move coming up in corner
+https://www.google.com/search?q=get+numberical+values+from+wav+file&ie=utf-8&oe=utf-8
+*/
